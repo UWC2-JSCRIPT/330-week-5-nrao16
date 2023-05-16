e712100a9f93af264021eb870c8f825d79391ca7
Nandini Rao [nandinirao] <nandinirao@costco.com>
2023-05-15 20:06:41 -0700

    added other routes and basic daos

--- daos/order.js
+++ daos/order.js
@@ -6,7 +6,6 @@ module.exports = {};
 
 module.exports.create = async (orderObj) => {
     try {
-        console.log(`orderObj - ${orderObj}`);
         return await Order.create(orderObj);
     } catch (e) {
         if (e.message.includes('validation failed') || e.message.includes('dup key')) {
@@ -15,3 +14,31 @@ module.exports.create = async (orderObj) => {
         throw e;
     }
 }
+
+module.exports.getById = async (orderId) => {
+    return Order.findOne({ _id: orderId }).lean();
+    // const _id = orderId;
+    // const orderWithITems = Order.aggregate([
+    //     {$match: { _id } },
+    //     { $lookup: {from: 'items', 
+    //     localField: '',
+    //     foreignField: '_id', 
+    //     as: 'item'} }
+    // ]);
+    // return orderWithUser;
+}
+
+module.exports.getByUserAndId = async (userId, orderId) => {
+    return Order.findOne({ userId: userId, _id: orderId }).lean();
+}
+
+module.exports.getByUserId = async (userId) => {
+    return Order.find({ userId: userId }).lean();
+}
+
+module.exports.getAll = async () => {
+    return Order.find().lean();
+}
+
+class BadDataError extends Error { };
+module.exports.BadDataError = BadDataError;
--- routes/orders.js
+++ routes/orders.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const { Router } = require("express");
 const router = Router();
 
@@ -12,32 +13,74 @@ router.use(isAuthorized);
 router.post("/", async (req, res, next) => {
     try {
         const orderItems = req.body;
-        if (!orderItems || !orderItems.length === 0) {
-            res.status(400).send('At least one item id is required');
+        if (!orderItems || !orderItems.length === 0 ||
+            orderItems.some(id => id === null)) {
+            res.status(400).send('Item id is required and has to be valid.');
         } else {
-            console.log(`orderItems - ${JSON.stringify(orderItems)}`);
+            //console.log(`orderItems - ${JSON.stringify(orderItems)}`);
 
             let totalPrice = 0;
             const mappedIds = [];
             for (const itemId of orderItems) {
-                console.log(`itemId - ${itemId._id}`)
-                const existingItem = await itemDAO.getById(itemId._id);
-                console.log(`existingItem - ${JSON.stringify(existingItem)}`)
+                //console.log(`itemId - ${itemId}`)
+                const existingItem = await itemDAO.getById(itemId);
+                //console.log(`existingItem - ${JSON.stringify(existingItem)}`)
                 if (existingItem) {
                     totalPrice += existingItem?.price;
                     mappedIds.push(existingItem._id);
+                } else {
+                    res.status(400).send(`Item id ${itemId} not found.`);
                 }
 
             }
+            //console.log(`mappedIds - ${mappedIds}`);
 
             const orderObj = { userId: req.user._id, items: mappedIds, total: totalPrice }
             console.log(`orderObj--${JSON.stringify(orderObj)}`);
             const savedOrder = await orderDAO.create(orderObj);
-            res.json({ savedOrder });
+            res.json(savedOrder);
         }
     } catch (e) {
         next(e);
     }
 });
+
+// Get single order for given order id
+router.get("/:id", async (req, res, next) => {
+    try {
+        let order = {};
+        if (req?.user?.roles?.includes('admin')) {
+            order = await orderDAO.getById(req.params.id);
+        } else {
+            order = await orderDAO.getByUserAndId(req.user._id, req.params.id);
+        }
+        console.log(`order -- ${JSON.stringify(order)}`);
+        if (order) {
+            res.json(order);
+        } else {
+            res.sendStatus(404);
+        }
+    } catch (e) {
+        next(e);
+    }
+});
+
+//  Get all items 
+router.get("/", async (req, res, next) => {
+    try {
+        console.log(`req.user._id: ${req.user._id}`);
+        let orders = [];
+        if (req?.user?.roles?.includes('admin')) {
+            orders = await orderDAO.getAll();
+        } else {
+            orders = await orderDAO.getByUserId(req.user._id);
+        }
+        console.log(`orders-${JSON.stringify(orders)}`);
+        res.json(orders);
+    } catch (e) {
+        next(e);
+    }
+});
+
 
 module.exports = router;
